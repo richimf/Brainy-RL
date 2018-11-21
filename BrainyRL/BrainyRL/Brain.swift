c3d252d838ebb86102256e2da0b0b636a9fbb793
Ricardo Montesinos Fernandez <ricardo.montesinos@globant.com>
2018-11-21 17:11:33 -0600

    Initial Project

---
+++ BrainyRL/BrainyRL/Brain.swift
@@ -0,0 +1,13 @@
+//
+//  Brain.swift
+//  BrainyRL
+//
+//  Created by Ricardo Montesinos Fernandez on 11/21/18.
+//  Copyright © 2018 CubitStudio. All rights reserved.
+//
+
+import UIKit
+
+class Brain: NSObject {
+
+}
