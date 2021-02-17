150de5fc116aad67265ffb37f57fb9ca374a6b2e
Ricardo Montesinos <ricardo.montesinos@payclip.com>
2021-02-17 12:50:34 -0600

    viewcontroller error

--- Examples/Hello/Hello/Hello/ViewController.swift
+++ Examples/Hello/Hello/Hello/ViewController.swift
@@ -5,26 +5,21 @@
 //  Created by Ricardo Montesinos Fernandez on 12/5/18.
 //  Copyright © 2018 CubitStudio. All rights reserved.
 //
-
 import UIKit
 import BrainyRL
 
 class ViewController: UIViewController {
 
-  let brain: Brainy = Brainy()
+  llet brain: Brainy = Brainy()
 
   override func viewDidLoad() {
     super.viewDidLoad()
     brain.setup()
     brain.environment
-    // Do any additional setup after loading the view, typically from a nib.
   }
 
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
-
-
 }
-
